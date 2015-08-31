e8f9c6526da500f382111c3910734ce4c610091f
Vaibhav Aggarwal <vaibhavagg98@gmail.com>
2015-08-30 23:03:41 -0700

    Forms: New base event class

---
+++ InteractApp/Event.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InteractApp
+{
+	public class Event
+	{
+		int Id;
+		String ImageUri;
+		String Name;
+		DateTime Date;
+		String Location;
+		String Desc;
+		List<String> Tags;
+
+		public Event (int EId, String EImageUri, String EName, DateTime EDate, String ELocation, String EDesc, List<String> ETags)
+		{
+			Id = EId;
+			ImageUri = EImageUri;
+			Name = EName;
+			Date = EDate;
+			Location = ELocation;
+			Desc = EDesc;
+			Tags = ETags;
+		}
+	}
+}
+
