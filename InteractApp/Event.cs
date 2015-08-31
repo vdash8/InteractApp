5180fe88831514ab5820b0e2d6fec6df606fcf15
Vaibhav Aggarwal <vaibhavagg98@gmail.com>
2015-08-31 16:35:27 -0700

    Event: added 'this' for constructor variable assignments I'm starting to remember C#/Java again!

--- InteractApp/Event.cs
+++ InteractApp/Event.cs
@@ -15,13 +15,13 @@ namespace InteractApp
 
 		public Event (int EId, String EImageUri, String EName, DateTime EDate, String ELocation, String EDesc, List<String> ETags)
 		{
-			Id = EId;
-			ImageUri = EImageUri;
-			Name = EName;
-			Date = EDate;
-			Location = ELocation;
-			Desc = EDesc;
-			Tags = ETags;
+			this.Id = EId;
+			this.ImageUri = EImageUri;
+			this.Name = EName;
+			this.Date = EDate;
+			this.Location = ELocation;
+			this.Desc = EDesc;
+			this.Tags = ETags;
 		}
 	}
 }
