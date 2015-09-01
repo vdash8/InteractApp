78dbe98a9d4d0c4efbeca41579d6649fcdd85aed
Vaibhav Aggarwal <vaibhavagg98@gmail.com>
2015-08-31 18:37:02 -0700

    Event: Use proper C# get/set so list binding works

--- InteractApp/Event.cs
+++ InteractApp/Event.cs
@@ -5,23 +5,25 @@ namespace InteractApp
 {
 	public class Event
 	{
-		public int Id;
-		public String ImageUri;
-		public String Name;
-		public DateTime Date;
-		public String Location;
-		public String Desc;
-		public List<String> Tags;
+		public int Id { get; set; }
+		public String ImageUri { get; set; }
+		public String Name { get; set; }
+		public DateTime Date { get; set; }
+		public String Location { get; set; }
+		public String Desc { get; set; }
+		public List<String> Tags { get; set; }
 
-		public Event (int EId, String EImageUri, String EName, DateTime EDate, String ELocation, String EDesc, List<String> ETags)
+		public static Event newEvent(int EId, String EImageUri, String EName, DateTime EDate, String ELocation, String EDesc, List<String> ETags)
 		{
-			this.Id = EId;
-			this.ImageUri = EImageUri;
-			this.Name = EName;
-			this.Date = EDate;
-			this.Location = ELocation;
-			this.Desc = EDesc;
-			this.Tags = ETags;
+			Event e = new Event ();
+			e.Id = EId;
+			e.ImageUri = EImageUri;
+			e.Name = EName;
+			e.Date = EDate;
+			e.Location = ELocation;
+			e.Desc = EDesc;
+			e.Tags = ETags;
+			return e;
 		}
 	}
 }
