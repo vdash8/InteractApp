56f40c45376f8eb47b24fd13b24564cf4a681833
Vaibhav Aggarwal <vaibhavagg98@gmail.com>
2015-09-02 21:31:11 -0700

    Events: attempting to use readonly fields for event data

--- InteractApp/Event.cs
+++ InteractApp/Event.cs
@@ -5,25 +5,22 @@ namespace InteractApp
 {
 	public class Event
 	{
-		public int Id { get; set; }
-		public String ImageUri { get; set; }
-		public String Name { get; set; }
-		public DateTime Date { get; set; }
-		public String Location { get; set; }
-		public String Desc { get; set; }
-		public List<String> Tags { get; set; }
+		public readonly int Id;
+		public readonly String ImageUri;
+		public readonly String Name;
+		public readonly DateTime Date;
+		public readonly String Location;
+		public readonly String Desc;
+		public readonly List<String> Tags;
 
-		public static Event newEvent(int EId, String EImageUri, String EName, DateTime EDate, String ELocation, String EDesc, List<String> ETags)
-		{
-			Event e = new Event ();
-			e.Id = EId;
-			e.ImageUri = EImageUri;
-			e.Name = EName;
-			e.Date = EDate;
-			e.Location = ELocation;
-			e.Desc = EDesc;
-			e.Tags = ETags;
-			return e;
+		public Event(int EId, String EImageUri, String EName, DateTime EDate, String ELocation, String EDesc, List<String> ETags) {
+			this.Id = EId;
+			this.ImageUri = EImageUri;
+			this.Name = EName;
+			this.Date = EDate;
+			this.Location = ELocation;
+			this.Desc = EDesc;
+			this.Tags = ETags;
 		}
 	}
 }
