38e7fb6f47f15c3946baaaf1e9ff40874b6cda20
Vaibhav Aggarwal <vaibhavagg98@gmail.com>
2015-09-02 23:23:27 -0700

    EventListPage: use XAML instead of C# for layouts

--- InteractApp/EventListPage.xaml.cs
+++ InteractApp/EventListPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 using Xamarin.Forms;
 
@@ -7,9 +8,22 @@ namespace InteractApp
 {
 	public partial class EventListPage : ContentPage
 	{
+		public List<Event> SampleList =  new List<Event> () {
+			Event.newEvent(0, "invalid_image_uri", "Event0", new DateTime(), "Fremont, CA", "Test Event 0", new List<String> (){ "service" })
+		};
+
 		public EventListPage ()
 		{
 			InitializeComponent ();
+			EventList.ItemsSource = SampleList;
+
+			EventList.ItemTapped += async (sender, e) => {
+				await DisplayAlert("Tapped", ((Event) e.Item).Name + " row was tapped", "OK");
+				Debug.WriteLine("Tapped: " + ((Event) e.Item).Name);
+				((ListView)sender).SelectedItem = null; // de-select the row
+			};
+
+			Padding = new Thickness (0,20,0,0);
 		}
 	}
 }
